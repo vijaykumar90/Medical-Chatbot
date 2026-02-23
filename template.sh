7464fc25a9332d5ef210e19a7a5353f0c486c43c
vijaykumar90 <vijay09041998@gmail.com>
2026-02-22 22:06:44 -0500

    folder structure added successfully

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creatinmg the directory structure for the project
+mkdir -p src
+mkdir -p research
+
+# Creating the files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipnyb
+touch requirements.txt
+
+
+echo "Project structure created successfully!"
